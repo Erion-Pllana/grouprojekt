cfab6476d25c51f20794cefe476ca1cf2907e280
Erion-Pllana <erionpllana35@gmail.com>
2025-05-19 22:58:56 +0200

    Add files via upload

---
+++ STORE PAGE/script.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerGroup: 1,
+    loop: true,
+    fade: true,
+    grabCursor: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      868: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1000: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      1250: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+
+
+  
+  function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+   
+    for (i = 0; i < x.length; i++) {
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display = "none";
+      }
+      else {
+        x[i].style.display = "list-item";
+      }
+    }
+  }
